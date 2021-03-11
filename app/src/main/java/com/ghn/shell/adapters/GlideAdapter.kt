4d49164570298c9dacf4bce1cc590442f26167e6
ghnam <ghnam@goodsflow.com>
2021-03-11 09:17:00 +0900

    BindingAdapter 추가

---
+++ app/src/main/java/com/ghn/shell/adapters/GlideAdapter.kt
@@ -0,0 +1,24 @@
+package com.ghn.shell.adapters
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+
+@BindingAdapter("imageFromId")
+fun bindImageFromId(view: ImageView, resId: Int) {
+    Glide.with(view.context)
+            .load(resId)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(view)
+}
+
+@BindingAdapter("imageFromUrl")
+fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
+    if (!imageUrl.isNullOrEmpty()) {
+        Glide.with(view.context)
+                .load(imageUrl)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(view)
+    }
+}
