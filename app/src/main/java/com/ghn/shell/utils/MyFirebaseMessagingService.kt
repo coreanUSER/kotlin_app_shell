0f8d01e4f9b5821cfe3d901be922a5b55be90cd2
ghnam <ghnam@goodsflow.com>
2021-03-12 17:05:41 +0900

    FirebaseMessagingService

--- app/src/main/java/com/ghn/shell/utils/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/ghn/shell/utils/MyFirebaseMessagingService.kt
@@ -16,7 +16,7 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-
+        
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
